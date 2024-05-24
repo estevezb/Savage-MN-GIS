2694dc004943f3e46864119dad8b4d6fff05443d
Brian Estevez <bestevez100@gmail.com>
2024-05-24 01:22:18 -0500

    revisions

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,7 +2,7 @@ from pelican import signals
 
 AUTHOR = 'Brian Estevez'
 SITENAME = 'Fly in the Ointment'
-SITEURL = 'https://estevezb.github.io/Fly-in-the-Ointment/'
+SITEURL = '/'
 
 ARTICLE_URL= 'articles/{slug}.html'
 ARTICLE_SAVE_AS = 'articles/{slug}.html'
