820eba3127c0459f48e528eac489b2f22f9f0e34
Brian Estevez <bestevez100@gmail.com>
2024-05-28 21:44:28 -0500

    update site url to reflect new repo name

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,7 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://estevezb.github.io/Fly-in-the-Ointment"
+SITEURL = "https://estevezb.github.io/Py-and-Sky-Labs"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
